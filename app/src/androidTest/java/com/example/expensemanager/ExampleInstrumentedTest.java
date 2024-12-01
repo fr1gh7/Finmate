f3dfd5ab458f9e1a9a9b993821b9f61a3260ff90
GOKUL PRAMOD <102728327+fr1gh7@users.noreply.github.com>
2024-12-02 00:39:27 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/expensemanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/expensemanager/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.example.expensemanager;
 
 import android.content.Context;
-
+ 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
