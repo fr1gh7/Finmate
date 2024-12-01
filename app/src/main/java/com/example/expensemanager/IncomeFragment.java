ed6e9af784034e10c150179f8fee6b6731c0fa2e
GOKUL PRAMOD <102728327+fr1gh7@users.noreply.github.com>
2024-12-02 00:43:00 +0530

    Update IncomeFragment.java

--- app/src/main/java/com/example/expensemanager/IncomeFragment.java
+++ app/src/main/java/com/example/expensemanager/IncomeFragment.java
@@ -8,7 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+ 
 import android.provider.ContactsContract;
 import android.provider.SearchRecentSuggestions;
 import android.view.LayoutInflater;
