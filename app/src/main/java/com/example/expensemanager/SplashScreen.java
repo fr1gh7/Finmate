8b8cdda8a83a3fb8a8aa33f743a15366df87fc05
GOKUL PRAMOD <102728327+fr1gh7@users.noreply.github.com>
2024-12-02 00:43:41 +0530

    Update SplashScreen.java

--- app/src/main/java/com/example/expensemanager/SplashScreen.java
+++ app/src/main/java/com/example/expensemanager/SplashScreen.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
+ 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
